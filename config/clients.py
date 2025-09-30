a7af6950d85a355d33ba28ce50189e622afa5532
Cezar Maldini <cezar.datascience@gmail.com>
2025-09-30 16:24:31 -0300

    Edição client qdrant

--- config/clients.py
+++ config/clients.py
@@ -4,7 +4,7 @@ from qdrant_client import QdrantClient
 # Client Qdrant
 def new_qdrant_client():
     url: str = st.secrets.get('QDRANT_URL')
-    api_key: str = st.secrets.get('QDRANT_URL')
+    api_key: str = st.secrets.get('QDRANT_API_KEY')
 
     qdrant = QdrantClient(url=url, api_key=api_key)
 
